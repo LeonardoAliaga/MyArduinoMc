62743dc418520eecf85d7e1226c61fddf2b45fa4
LeonardoAliaga <aliagaleonardo23@gmail.com>
2025-11-17 08:51:55 -0500

    update

---
+++ src/client/java/com/myarduinomc/MyArduinoMcClient.java
@@ -0,0 +1,10 @@
+package com.myarduinomc;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MyArduinoMcClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
